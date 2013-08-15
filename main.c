3d6ef574f134d9612e2f7606f068684b4282a950
Ben Tatman <ben@tatmans.co.uk>
2013-08-15 09:05:58 +0100

    Final commit for now. Added license and welcome message

--- main.c
+++ main.c
@@ -1,3 +1,22 @@
+/*
+Curiosity. Insanely simple adventure game, written in less than 12 hours (day project, I also did other stuff in that day).
+Copyright (C) 2013  Ben Tatman
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include "defines.c"
@@ -21,6 +40,8 @@ int main(int argc, char *argv[]) {
 	char input[50];
 	player.water = 500;
 	player.food = 500;
+	printf("Curiosity version 1, Copyright (C) 2013 Ben Tatman\nCuriosity comes with ABSOLUTELY NO WARRANTY.  This is \nfree software, and you are welcome to redistribute it under \ncertain conditions. Look in the LICENSE file for more info.\n");
+	printf("You are in a field in the middle of the country side, with no food or water to bear. You see mountains, and streams. Find some water and food.\nCommands avaliable are: \n\tn = North\n\tne = North East\n\t(And so on)\n\ttake = Takes items\n\tinventory = Takes inventory.\n");
 	while (1==1) {
 		printf("> ");
 		scanf("%49s", input);
--- map_gen.c
+++ map_gen.c
@@ -1,3 +1,21 @@
+/*
+Curiosity. Insanely simple adventure game, written in less than 12 hours (day project, I also did other stuff in that day).
+Copyright (C) 2013  Ben Tatman
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+*/
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -53,7 +71,7 @@ void cave(char *map, int currpos) {
 			case 1:
 				water.pos[water.count] = to_position(currpos);
 				water.count++;
-				printf("Water: %d/%d\n", water.pos[water.count-1].x, water.pos[water.count-1].y);
+
 				break;
 			case 2:
 				food.pos[food.count] = to_position(currpos);
